7e9e3093cc0729094d2601d364a3b44255f5623f
Jaytee <Padilla.Jaytee@gmail.com>
2023-08-31 10:02:52 -0700

    Added password hashing when creating user

--- models/User.js
+++ models/User.js
@@ -1,21 +1,37 @@
 const { Schema, model } = require('mongoose');
+const bcrypt = require('bcrypt');
 
 const userSchema = new Schema({
   email: {
     type: String,
     required: true,
     unique: true,
-    trim: true
+    trim: true,
   },
   password: {
     type: String,
-    required: true
+    required: true,
   },
   createdAt: {
     type: Date,
-    default: Date.now()
+    default: Date.now(),
+  },
+});
+
+userSchema.pre('save', async function(next) {
+  // only hash the password if it has been modified (or is new)
+  if(this.isModified('password')) {
+    this.password = await bcrypt.hash(this.password, 12);
   }
 });
+
+userSchema.methods.comparePassword = function(candidatePassword, cb) {
+  bcrypt.compare(candidatePassword, this.password, (err, isMatch) => {
+    if (err) return cb(err);
+
+    cb(null, isMatch);
+  });
+};
 
 const UserModel = model('User', userSchema);
 
